ac54441731113b7f636fae1be7ee7dd46c622ea6
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-06-02 20:21:38 +0200

    header title changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <a className="navbar-brand" href="/">
           <div className="d-flex">
             <img src={logo} alt="logo" className="mr-2" />
-            <div>ProjectManagement</div>
+            <div>Project Management</div>
           </div>
         </a>
       </div>
